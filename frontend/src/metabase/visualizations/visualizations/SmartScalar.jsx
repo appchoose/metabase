87aa0868a5e371718b24c7917fd56f84d34d1a87
Séverin Gouge <severin@appchoose.io>
2023-07-21 14:22:14 +0200

    feat: add goals as questions fix: change log color

--- frontend/src/metabase/visualizations/visualizations/SmartScalar.jsx
+++ frontend/src/metabase/visualizations/visualizations/SmartScalar.jsx
@@ -9,7 +9,7 @@ import { color } from "metabase/lib/colors";
 import Icon from "metabase/components/Icon";
 
 import { columnSettings } from "metabase/visualizations/lib/settings/column";
-import { NoBreakoutError } from "metabase/visualizations/lib/errors";
+// import { NoBreakoutError } from "metabase/visualizations/lib/errors";
 import { compactifyValue } from "metabase/visualizations/lib/scalar_utils";
 
 import ScalarValue, {
@@ -64,7 +64,7 @@ export default class Smart extends React.Component {
   };
 
   static isSensible({ insights }) {
-    return insights && insights.length > 0;
+    return true;
   }
 
   // Smart scalars need to have a breakout
@@ -77,9 +77,9 @@ export default class Smart extends React.Component {
     settings,
   ) {
     if (!insights || insights.length === 0) {
-      throw new NoBreakoutError(
-        t`Group by a time field to see how this has changed over time`,
-      );
+      // use Choose custom view
+      console.log("Switching to Choose custom view");
+      return true;
     }
   }
 
@@ -114,42 +114,79 @@ export default class Smart extends React.Component {
     const insights =
       rawSeries && rawSeries[0].data && rawSeries[0].data.insights;
     const insight = _.findWhere(insights, { col: column.name });
+
+    const isSwapped = settings["scalar.switch_positive_negative"];
+
+    let lastValue;
+    const trends = [];
     if (!insight) {
-      return null;
+      // Choose custom smartScalar
+      const cols = rawSeries[0].data?.cols;
+      const bucketIdx = _.findIndex(cols, col => col.base_type === "type/Text");
+      if (bucketIdx > -1) {
+        const valueIdx = _.findIndex(
+          cols,
+          val => val.display_name === column.name,
+        );
+        lastValue = rawSeries[0].data.rows[0][valueIdx];
+        rawSeries[0].data.rows.forEach((row, idx) => {
+          if (!idx) {
+            return;
+          }
+          const trend = {
+            granularity: formatBucketing(row[bucketIdx]).toLowerCase(),
+            previousValue: row[valueIdx],
+            lastChange: (lastValue - row[valueIdx]) / row[valueIdx],
+          };
+          trend.isNegative = trend.lastChange < 0;
+          trend.changeColor = (isSwapped ? !trend.isNegative : trend.isNegative)
+            ? color("error")
+            : color("success");
+          trends.push(trend);
+        });
+      } else {
+        return null;
+      }
+    } else {
+      const granularity = formatBucketing(insight["unit"]).toLowerCase();
+
+      const lastChange = insight["last-change"];
+      const previousValue = insight["previous-value"];
+
+      const isNegative = lastChange < 0;
+
+      // if the number is negative but thats been identified as a good thing (e.g. decreased latency somehow?)
+      const changeColor = (isSwapped ? !isNegative : isNegative)
+        ? color("error")
+        : color("success");
+      trends.push({
+        granularity,
+        lastChange,
+        previousValue,
+        isNegative,
+        changeColor,
+      });
+
+      lastValue = insight["last-value"];
     }
 
-    const lastValue = insight["last-value"];
     const formatOptions = settings.column(column);
-
     const { displayValue, fullScalarValue } = compactifyValue(
       lastValue,
       width,
       formatOptions,
     );
 
-    const granularity = formatBucketing(insight["unit"]).toLowerCase();
-
-    const lastChange = insight["last-change"];
-    const previousValue = insight["previous-value"];
-
-    const isNegative = lastChange < 0;
-    const isSwapped = settings["scalar.switch_positive_negative"];
-
-    // if the number is negative but thats been identified as a good thing (e.g. decreased latency somehow?)
-    const changeColor = (isSwapped ? !isNegative : isNegative)
-      ? color("error")
-      : color("success");
-
-    const changeDisplay = (
+    const changeDisplay = value => (
       <span style={{ fontWeight: 900 }}>
-        {formatNumber(Math.abs(lastChange), { number_style: "percent" })}
+        {formatNumber(Math.abs(value), { number_style: "percent" })}
       </span>
     );
     const separator = (
       <PreviousValueSeparator gridSize={gridSize}>•</PreviousValueSeparator>
     );
-    const granularityDisplay = (
-      <span style={{ marginLeft: 5 }}>{jt`last ${granularity}`}</span>
+    const granularityDisplay = value => (
+      <span style={{ marginLeft: 5 }}>{jt`last ${value}`}</span>
     );
 
     const clicked = {
@@ -210,30 +247,34 @@ export default class Smart extends React.Component {
           />
         )}
         <div className="SmartWrapper">
-          {lastChange == null || previousValue == null ? (
+          {!trends.length ? (
             <div
               className="text-centered text-bold mt1"
-              style={{ color: color("text-medium") }}
-            >{jt`Nothing to compare for the previous ${granularity}.`}</div>
-          ) : lastChange === 0 ? (
-            t`No change from last ${granularity}`
+              color={color("text-medium")}
+            >{jt`Nothing to compare for the previous .`}</div>
           ) : (
-            <PreviousValueContainer gridSize={gridSize}>
-              <Variation color={changeColor}>
-                <Icon
-                  size={13}
-                  pr={1}
-                  name={isNegative ? "arrow_down" : "arrow_up"}
-                />
-                {changeDisplay}
-              </Variation>
-              <PreviousValueVariation id="SmartScalar-PreviousValue">
-                {jt`${separator} was ${formatValue(
-                  previousValue,
-                  settings.column(column),
-                )} ${granularityDisplay}`}
-              </PreviousValueVariation>
-            </PreviousValueContainer>
+            <div>
+              {trends.map((trend, idx) => {
+                return (
+                  <PreviousValueContainer key={idx}>
+                    <Variation color={trend.changeColor}>
+                      <Icon
+                        size={13}
+                        pr={1}
+                        name={trend.isNegative ? "arrow_down" : "arrow_up"}
+                      />
+                      {changeDisplay(trend.lastChange)}
+                    </Variation>
+                    <PreviousValueVariation id="SmartScalar-PreviousValue">
+                      {jt`${separator} was ${formatValue(
+                        trend.previousValue,
+                        settings.column(column),
+                      )} ${granularityDisplay(trend.granularity)}`}
+                    </PreviousValueVariation>
+                  </PreviousValueContainer>
+                );
+              })}
+            </div>
           )}
         </div>
       </ScalarWrapper>
